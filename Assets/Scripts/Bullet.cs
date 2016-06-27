60a4a890b441b5cf106020afc6c363efc2f8fcc8
Pitri-Mac <pjoetri@gmail.com>
2016-06-27 20:13:03 +0200

    ulu

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,8 +3,17 @@ using System.Collections;
 
 public class Bullet : MonoBehaviour {
 
-    void OnCollisionEnter()
+	void OnCollisionEnter(Collision collision)
     {
+		GameObject hit = collision.gameObject;
+		if (hit.tag != "Player")
+			return;
+		
+		
+		Health health = hit.GetComponent<Health> ();
+		if (health != null)
+			health.TakeDamage (10);
+		
         Destroy(gameObject);
     }
 
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+
+
+	public const int maxHealth = 100;
+	public int currentHealth = maxHealth;
+	public RectTransform healthBar;
+
+
+	public void TakeDamage(int val) {
+		currentHealth -= val;
+
+		healthBar.sizeDelta = new Vector2 (currentHealth, healthBar.sizeDelta.y);
+
+		if (currentHealth <= 0) {
+			currentHealth = 0;
+			Debug.Log ("Dead!");
+		}
+	}
+
+
+}
